b567efcfe7ba369e6aa31979f03552ff4da7f4d7
Lidia Gianne <lidiagianne@gmail.com>
2019-05-11 04:13:35 -0300

    Update environment.ts

--- common/environment.ts
+++ common/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  server: {port: process.env.MONGOLAB_URI || 3000},
+  server: {port: process.env.SERVER_PORT  || 3000},
   db: {url: process.env.DB_URL || 'mongodb://localhost/nailhear'},
   security: {saltRounds: process.env.SALT_ROUNDS || 10},
   
